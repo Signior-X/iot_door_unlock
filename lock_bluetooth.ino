a2d34246c185c1709bf79c3e7f55cbcf78bf33be
Priyam <56730716+Signior-X@users.noreply.github.com>
2021-03-01 19:43:03 +0530

    Update lock_bluetooth.ino

--- lock_bluetooth.ino
+++ lock_bluetooth.ino
@@ -1,51 +1,44 @@
 char t;
- 
+
+#include <Servo.h>
+Servo servo;
+int angle = 10;
+
 void setup() {
-pinMode(13,OUTPUT);   //left motors forward
-pinMode(12,OUTPUT);   //left motors reverse
-pinMode(11,OUTPUT);   //right motors forward
-pinMode(10,OUTPUT);   //right motors reverse
-pinMode(9,OUTPUT);   //Led
-Serial.begin(9600);
- 
-}
- 
-void loop() {
-if(Serial.available()){
-  t = Serial.read();
-  Serial.println(t);
-}
- 
-if(t == 'F'){            //move forward(all motors rotate in forward direction)
-  digitalWrite(13,HIGH);
-  digitalWrite(11,HIGH);
-}
- 
-else if(t == 'B'){      //move reverse (all motors rotate in reverse direction)
-  digitalWrite(12,HIGH);
-  digitalWrite(10,HIGH);
-}
- 
-else if(t == 'L'){      //turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
-  digitalWrite(11,HIGH);
-}
- 
-else if(t == 'R'){      //turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
-  digitalWrite(13,HIGH);
+
+  servo.attach(9);
+  servo.write(angle);
+
+  Serial.begin(9600);
+
 }
 
-else if(t == 'W'){    //turn led on or off)
-  digitalWrite(9,HIGH);
-}
-else if(t == 'w'){
-  digitalWrite(9,LOW);
-}
- 
-else if(t == 'S'){      //STOP (all motors stop)
-  digitalWrite(13,LOW);
-  digitalWrite(12,LOW);
-  digitalWrite(11,LOW);
-  digitalWrite(10,LOW);
-}
-delay(100);
+void loop() {
+  if (Serial.available()) {
+    t = Serial.read();
+    Serial.println(t);
+  }
+
+  if (t == 'w') {          //move forward(all motors rotate in forward direction)
+    Serial.println("hello");
+
+    for (angle = 10; angle < 180; angle++)
+    {
+      servo.write(angle);
+      delay(15);
+    }
+  }
+
+  else if (t == 'a') {    //move reverse (all motors rotate in reverse direction)
+    Serial.println("Bye");
+
+    for (angle = 180; angle > 10; angle--)
+    {
+      servo.write(angle);
+      delay(15);
+    }
+  }
+
+
+  delay(100);
 }
